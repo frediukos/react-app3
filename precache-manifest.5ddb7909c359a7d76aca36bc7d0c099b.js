5d9927c456cab50623e415190139fe633a69118a
olenafrediuk <frediuk_olena@ukr.net>
2020-05-23 19:48:43 +0300

    Updates

---
+++ precache-manifest.5ddb7909c359a7d76aca36bc7d0c099b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d089165cc6b730e10c5e68bbb3577e",
+    "url": "/react-app3/index.html"
+  },
+  {
+    "revision": "c291b6b0d06d3648a6a7",
+    "url": "/react-app3/static/css/main.8bc69b89.chunk.css"
+  },
+  {
+    "revision": "4d6b2d4796fab839fe1a",
+    "url": "/react-app3/static/js/2.dc9b9f87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app3/static/js/2.dc9b9f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c291b6b0d06d3648a6a7",
+    "url": "/react-app3/static/js/main.2fe7c926.chunk.js"
+  },
+  {
+    "revision": "0770771e6e4cfd148162",
+    "url": "/react-app3/static/js/runtime-main.49b81f11.js"
+  },
+  {
+    "revision": "8248b08ac468db7d640b6daa5c5a4681",
+    "url": "/react-app3/static/media/logo_alex_circle2.8248b08a.png"
+  }
+]);
